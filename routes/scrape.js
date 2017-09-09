a0d19c1b6c9622e04ae7b3bac4ee97e6f432b0c8
erick <erick.manantan@gmail.com>
2017-09-09 10:40:00 +0800

    new

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,29 +1,29 @@
-var express = require('express')
-var router = express.Router()
-var superagent = require('superagent')
+// var express = require('express')
+// var router = express.Router()
+// var superagent = require('superagent')
 
 
-router.get('/', function(req, res, next) {
+// router.get('/', function(req, res, next) {
 
-	superagent
-	.get(url)
-	.query()
-	.end(function(err, response){
-		if (err){
-			res.json({
-				confirmation: 'fail',
-				message:err
-			})
-			return
-		}
-		res.send(response.text)
-	})
+// 	superagent
+// 	.get(url)
+// 	.query()
+// 	.end(function(err, response){
+// 		if (err){
+// 			res.json({
+// 				confirmation: 'fail',
+// 				message:err
+// 			})
+// 			return
+// 		}
+// 		res.send(response.text)
+// 	})
 
-  // res.json ({
-  // 	confirmation:'success'
+//   // res.json ({
+//   // 	confirmation:'success'
 
-  // })
+//   // })
 
-})
+// })
 
-module.exports = router;
+// module.exports = router;
